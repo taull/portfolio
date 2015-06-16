7f895881d3b554cf7301e40aa93c22e3d72c8228
Taylor Aull <aull.taylor@gmail.com>
2015-06-16 17:37:52 -0400

    cleaned up the site, implemented fullpage.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,3 @@
-$(document).ready(function(){
-
-  $('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
-
-
-// Add/Remove Project Slideshow
-
-$(function() {
-  $(".clickable1").click(function() {
-
-      $('.banner').addClass("hidden", {duration:2000,effect:'blind'});
-
-      $('.project1').removeClass("hidden", {duration:2000,effect:'blind'});
-
-      $('.project2').addClass("hidden", {duration:2000,effect:'blind'});
-
-  });
-});
-
-$(function() {
-  $(".clickable2").click(function() {
-
-      $('.banner').addClass("hidden");
-
-      $('.project2').removeClass("hidden");
-
-      $('.project1').addClass("hidden");
-
-  });
-});
 
 // Banner typing letter
 
@@ -66,20 +11,59 @@ $(function(){
     });
 });
 
-//Project Header Fixed Scroll
+// FullPage.js
 
-$('.container').stickem('start: 100px');
+  $(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['home', 'services', 'contact'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide'],
+        showActiveTooltips: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
 
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 1500,
+        autoScrolling: true,
+        fitToSection: true,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
 
-// Reverse Color Scheme
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
 
-$('.my-logo').on('click', function(e) {
-      $('.project-wrap').toggleClass("project-wrap-reverse");
-      $('.my-logo').toggleClass("my-logo-blue");
-      $('.element').toggleClass("element-reverse");
-      $('.reverse-button').toggleClass("reverse-reverse");
-      $('.header-nav').toggleClass("header-nav-reverse");
-      $('.navbar').toggleClass("navbar-inverse");
+        //Design
+        controlArrows: true,
+        verticalCentered: false,
+        resize : false,
+        sectionsColor : ['#222', '#f0f0f0', '#f8f8f8'],
+        fixedElements: '#header, .footer',
+        responsive: 1080,
 
-      e.preventDefault();
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
     });
+});
